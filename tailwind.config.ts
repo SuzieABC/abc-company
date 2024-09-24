9b4ebb3caa44a55cc0f755e27d5471c0c71e1811
ahnlab <ahnlab@ahnlabui-MacBookPro.local>
2024-09-24 10:02:41 +0900

    feat: add Outfit font

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,9 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      fontFamily:{
+        custom: ['Outfit', 'sans-serif']
+      },
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
