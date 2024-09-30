3d9552e57398492cd3b769e87aa2cfa8bd15234f
SuzieABC <suzie@ahnlab.io>
2024-09-30 17:45:56 +0900

    feat: set the layouts with components

--- src/app/[locale]/company/page.tsx
+++ src/app/[locale]/company/page.tsx
@@ -1,5 +1,9 @@
 import { createTranslation } from "@/utils/localization/server";
 import { LocaleTypes } from "@/utils/localization/settings";
+import Media from "@/components/company/Media";
+import Partners from "@/components/company/Partners";
+import TopPage from "@/components/company/TopPage";
+import BottomPage from "@/components/company/BottomPage";
 
 export default async function Page({
   params: { locale },
@@ -10,8 +14,11 @@ export default async function Page({
   const { t } = await createTranslation(locale, "company");
 
   return (
-    <div className="flex flex-col justify-center items-center">
-      <span className="text-white text-[70px] uppercase">{t("title")}</span>
+    <div className="flex flex-col justify-center items-center text-white">
+      <TopPage desc={t("description")} />
+      <Partners />
+      <Media button={t(`load_more_button`)} />
+      <BottomPage desc={t("partner_description")} />
     </div>
   );
 }
--- src/app/[locale]/products/page.tsx
+++ src/app/[locale]/products/page.tsx
@@ -1,5 +1,9 @@
 import { createTranslation } from "@/utils/localization/server";
 import { LocaleTypes } from "@/utils/localization/settings";
+import MainTopPage from "@/components/products/section/MainTopPage";
+import MainCompanyInfo from "@/components/products/section/MainCompanyInfo";
+import Products from "@/components/products/section/Products";
+import AboutUs from "@/components/products/section/AboutUs";
 
 export default async function Page({
   params: { locale },
@@ -10,8 +14,26 @@ export default async function Page({
   const { t } = await createTranslation(locale, "products");
 
   return (
-    <div className="flex flex-col justify-center items-center">
-      <span className="text-white text-[70px] uppercase">{t("title")}</span>
+    <div className="flex flex-col justify-center items-center text-center">
+      <MainTopPage title={t("title")} subTitle={t("subtitle")} />
+      <MainCompanyInfo
+        company_name={t("company_name")}
+        company_detail={t("company_detail")}
+      />
+      <Products
+        walletTitle={t(`item.wallet.title`)}
+        walletHighlight={t("item.wallet.highlight")}
+        walletDetail={t("item.wallet.detail")}
+        waasTitle={t(`item.waas.title`)}
+        waasHighlight={t("item.waas.highlight")}
+        waasDetail={t("item.waas.detail")}
+        bicTitle={t(`item.BIC.title`)}
+        bicHighlight={t("item.BIC.highlight")}
+        bicDetail={t("item.BIC.detail")}
+        kytTitle={t("item.KYT.title")}
+        kytHighlight={t("item.KYT.highlight")}
+      />
+      <AboutUs buttonText={t("about_us_button")} />
     </div>
   );
 }
