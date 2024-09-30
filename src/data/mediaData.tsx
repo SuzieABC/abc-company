9b2565cef56794a23a56446d38d67dfdba77d0e9
SuzieABC <suzie@ahnlab.io>
2024-09-30 17:41:42 +0900

    feat: added datas for media and partners

---
+++ src/data/mediaData.tsx
@@ -0,0 +1,77 @@
+import media_1 from "@/assets/images/company/media/media_1.png";
+import media_2 from "@/assets/images/company/media/media_2.png";
+import media_3 from "@/assets/images/company/media/media_3.png";
+import media_4 from "@/assets/images/company/media/media_4.png";
+import media_5 from "@/assets/images/company/media/media_5.png";
+import media_6 from "@/assets/images/company/media/media_6.png";
+import media_7 from "@/assets/images/company/media/media_7.png";
+import media_8 from "@/assets/images/company/media/media_8.png";
+
+const MEDIA_LIST = [
+  {
+    id: 8,
+    title: "안랩블록체인컴퍼니 “ABC월렛, 웹2 사용자들의 쉬운 온보딩 돕겠다”",
+    date: "2024.07.09",
+    tag: "News",
+    url: "https://bloomingbit.io/feed/news/71267",
+    image: media_8,
+  },
+  {
+    id: 7,
+    title: "서울관광재단, ‘2024 서울썸머비치’ 개최…우선입장권 응모 이벤트 진행",
+    date: "2024.07.08",
+    tag: "News",
+    url: "http://seoulca.co.kr/View.aspx?No=3293405",
+    image: media_7,
+  },
+  {
+    id: 6,
+    title: "에그버스, 안랩블록체인컴퍼니와 ‘ABC Wallet’ 연동 파트너십 체결",
+    date: "2024.06.07",
+    tag: "News",
+    url: "https://www.paxetv.com/news/articleView.html?idxno=204782",
+    image: media_6,
+  },
+  {
+    id: 5,
+    title: "안랩블록체인컴퍼니, 웁살라시큐리티와 가상자산 AML 보안 기술 협력",
+    date: "2024.05.28",
+    tag: "News",
+    url: "https://ddaily.co.kr/page/view/2024052809210325295",
+    image: media_5,
+  },
+  {
+    id: 4,
+    title: "서강대, 안랩블록체인컴퍼니와 NFT티켓 시스템 개발",
+    date: "2024.05.14",
+    tag: "News",
+    url: "https://it.chosun.com/news/articleView.html?idxno=2023092115712",
+    image: media_4,
+  },
+  {
+    id: 3,
+    title: "안랩블록체인컴퍼니, 뮤지컬 ‘스쿨 오브 락’ 월드투어 기념 NFT 증정",
+    date: "2024.01.29",
+    tag: "News",
+    url: "https://zdnet.co.kr/view/?no=20240129095504",
+    image: media_3,
+  },
+  {
+    id: 2,
+    title: "서강대 메타버스 교육 플랫폼에 안랩블록체인 기술 연동",
+    date: "2023.12.20",
+    tag: "News",
+    url: "https://n.news.naver.com/mnews/article/003/0012278168?sid=105&source=post_page-----b5b304b9a445--------------------------------",
+    image: media_2,
+  },
+  {
+    id: 1,
+    title: "안랩블록체인컴퍼니, ‘서울빛초롱축제’ 기념 NFT 발행",
+    date: "2023.12.14",
+    tag: "News",
+    url: "https://zdnet.co.kr/view/?no=20231214100727",
+    image: media_1,
+  },
+];
+
+export default MEDIA_LIST;
---
+++ src/data/partnersData.tsx
@@ -0,0 +1,28 @@
+import SKTelecom from "@/assets/images/company/partners_logo/SK-telecom.svg";
+import SogangUniv from "@/assets/images/company/partners_logo/SogangUniv.svg";
+import STO from "@/assets/images/company/partners_logo/STO.svg";
+import Kaia from "@/assets/images/company/partners_logo/Kaia.svg";
+import Uppsala from "@/assets/images/company/partners_logo/Uppsala.svg";
+import Chainlink from "@/assets/images/company/partners_logo/Chainlink.svg";
+import Colligence from "@/assets/images/company/partners_logo/Colligence.svg";
+import TokenPost from "@/assets/images/company/partners_logo/TokenPost.svg";
+import OneInch from "@/assets/images/company/partners_logo/oneInch.svg";
+
+const partnersList = [
+  { id: "sk_telecom", name: "SK telecom", image: SKTelecom, width: 71 },
+  { id: "sogang_university", name: "서강대학교", image: SogangUniv, width: 85 },
+  { id: "sto", name: "서울관광재단", image: STO, width: 54 },
+  { id: "kaia", name: "Kaia", image: Kaia, width: 55 },
+  {
+    id: "uppsala_security",
+    name: "uppsalasecurity",
+    image: Uppsala,
+    width: 93,
+  },
+  { id: "Chainlink", name: "Chainlink", image: Chainlink, width: 73 },
+  { id: "Colligence", name: "Colligence", image: Colligence, width: 73 },
+  { id: "Tokenpost", name: "Tokenpost", image: TokenPost, width: 84 },
+  { id: "oneInch", name: "1inch", image: OneInch, width: 63 },
+];
+
+export default partnersList;
