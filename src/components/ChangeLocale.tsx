7ec385836c1d3c01c02a61dfb780345d2006d029
SuzieABC <suzie@ahnlab.io>
2024-10-11 09:31:37 +0900

    feat: added icon with black colour

--- src/components/ChangeLocale.tsx
+++ src/components/ChangeLocale.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import {
   usePathname,
   /* useParams, */
@@ -8,13 +8,19 @@ import {
   useSelectedLayoutSegments,
 } from "next/navigation";
 import worldIcon from "@/assets/icons/world_icon.png";
+import worldIcon_colour from "@/assets/icons/world_light.svg";
 import Image from "next/image";
 
-const ChangeLocale = () => {
+interface ChnageLocaleProps {
+  bgColor: string;
+}
+
+const ChangeLocale = ({ bgColor }: ChnageLocaleProps) => {
   const router = useRouter();
   /* const params = useParams(); */
   const urlSegments = useSelectedLayoutSegments();
   const [isDropdownOpen, setIsDropdownOpen] = useState(false); // 드롭다운 열림 여부 상태
+  const dropdownRef = useRef<HTMLDivElement | null>(null);
 
   const handleLocaleChange = (newLocale: string) => {
     // 언어 변경 후 드롭다운 닫기
@@ -28,19 +34,47 @@ const ChangeLocale = () => {
 
   const pathname = usePathname();
 
+  useEffect(() => {
+    const handleClickOutside = (e: MouseEvent) => {
+      if (
+        dropdownRef.current &&
+        !dropdownRef.current.contains(e.target as Node)
+      ) {
+        setIsDropdownOpen(false);
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [dropdownRef]);
+
   return (
     <div className="px-5 relative">
       {/* 공 모양 이미지 버튼 */}
-      <Image
-        src={worldIcon} // 원하는 공 모양 이미지 경로로 수정
-        alt=""
-        className="w-[24px] h-[24px] cursor-pointer"
-        onClick={toggleDropdown}
-      />
+      {bgColor === "transparent" ? (
+        <Image
+          src={worldIcon} // 원하는 공 모양 이미지 경로로 수정
+          alt=""
+          className="w-[24px] h-[24px] cursor-pointer"
+          onClick={toggleDropdown}
+        />
+      ) : (
+        <Image
+          src={worldIcon_colour} // 원하는 공 모양 이미지 경로로 수정
+          alt=""
+          className="w-[24px] h-[24px] cursor-pointer"
+          onClick={toggleDropdown}
+        />
+      )}
 
       {/* 드롭다운 메뉴 */}
       {isDropdownOpen && (
-        <div className="absolute top-10 left-[-17px] h-[88px] px-3 bg-white/90 rounded-xl shadow flex-col justify-center items-start inline-flex">
+        <div
+          className="absolute top-10 left-[-17px] h-[88px] px-3 bg-white/90 rounded-xl shadow flex-col justify-center items-start inline-flex"
+          ref={dropdownRef}
+        >
           <div
             className="w-20 h-11 py-3 justify-center items-center gap-2.5 inline-flex cursor-pointer"
             onClick={() => handleLocaleChange("ko")}
