9aa54bd943c42fa19c7813d7d2317988e5e4835e
SuzieABC <suzie@ahnlab.io>
2024-10-14 19:56:26 +0900

    fix: changed images to png due to resolution issue on ios devices

--- src/data/partnersData.tsx
+++ src/data/partnersData.tsx
@@ -1,38 +1,75 @@
-// import SKTelecom from "@/assets/images/company/partners_logo/SK-telecom.svg";
-// import SogangUniv from "@/assets/images/company/partners_logo/SogangUniv.svg";
-// import STO from "@/assets/images/company/partners_logo/STO.svg";
-// import Kaia from "@/assets/images/company/partners_logo/Kaia.svg";
-// import Uppsala from "@/assets/images/company/partners_logo/Uppsala.svg";
-// import Chainlink from "@/assets/images/company/partners_logo/Chainlink.svg";
-// import Colligence from "@/assets/images/company/partners_logo/Colligence.svg";
-// import TokenPost from "@/assets/images/company/partners_logo/TokenPost.svg";
-// import OneInch from "@/assets/images/company/partners_logo/oneInch.svg";
+import SKTelecom from "@/assets/images/company/partners_logo/1.png";
+import SogangUniv from "@/assets/images/company/partners_logo/2.png";
+import STO from "@/assets/images/company/partners_logo/3.png";
+import Kaia from "@/assets/images/company/partners_logo/4.png";
+import Uppsala from "@/assets/images/company/partners_logo/5.png";
+import Chainlink from "@/assets/images/company/partners_logo/6.png";
+import Colligence from "@/assets/images/company/partners_logo/7.png";
+import TokenPost from "@/assets/images/company/partners_logo/8.png";
+import OneInch from "@/assets/images/company/partners_logo/9.png";
 
-import SKTelecom from "@/assets/images/company/partners_logo/1.svg";
-import SogangUniv from "@/assets/images/company/partners_logo/2.svg";
-import STO from "@/assets/images/company/partners_logo/3.svg";
-import Kaia from "@/assets/images/company/partners_logo/4.svg";
-import Uppsala from "@/assets/images/company/partners_logo/5.svg";
-import Chainlink from "@/assets/images/company/partners_logo/6.svg";
-import Colligence from "@/assets/images/company/partners_logo/7.svg";
-import TokenPost from "@/assets/images/company/partners_logo/8.svg";
-import OneInch from "@/assets/images/company/partners_logo/9.svg";
+import SKTelecom_s from "@/assets/images/company/partners_logo/1_s.png";
+import SogangUniv_s from "@/assets/images/company/partners_logo/2_s.png";
+import STO_s from "@/assets/images/company/partners_logo/3_s.png";
+import Kaia_s from "@/assets/images/company/partners_logo/4_s.png";
+import Uppsala_s from "@/assets/images/company/partners_logo/5_s.png";
+import Chainlink_s from "@/assets/images/company/partners_logo/6_s.png";
+import Colligence_s from "@/assets/images/company/partners_logo/7_s.png";
+import TokenPost_s from "@/assets/images/company/partners_logo/8_s.png";
+import OneInch_s from "@/assets/images/company/partners_logo/9_s.png";
 
 const partnersList = [
-  { id: "sk_telecom", name: "SK telecom", image: SKTelecom, width: 71 },
-  { id: "sogang_university", name: "서강대학교", image: SogangUniv, width: 85 },
-  { id: "sto", name: "서울관광재단", image: STO, width: 54 },
-  { id: "kaia", name: "Kaia", image: Kaia, width: 55 },
+  {
+    id: "sk_telecom",
+    name: "SK telecom",
+    image: SKTelecom,
+    image_s: SKTelecom_s,
+    width: 71,
+  },
+  {
+    id: "sogang_university",
+    name: "서강대학교",
+    image: SogangUniv,
+    image_s: SogangUniv_s,
+    width: 85,
+  },
+  { id: "sto", name: "서울관광재단", image: STO, image_s: STO_s, width: 54 },
+  { id: "kaia", name: "Kaia", image: Kaia, image_s: Kaia_s, width: 55 },
   {
     id: "uppsala_security",
     name: "uppsalasecurity",
     image: Uppsala,
+    image_s: Uppsala_s,
     width: 93,
   },
-  { id: "Chainlink", name: "Chainlink", image: Chainlink, width: 73 },
-  { id: "Colligence", name: "Colligence", image: Colligence, width: 73 },
-  { id: "Tokenpost", name: "Tokenpost", image: TokenPost, width: 84 },
-  { id: "oneInch", name: "1inch", image: OneInch, width: 63 },
+  {
+    id: "Chainlink",
+    name: "Chainlink",
+    image: Chainlink,
+    image_s: Chainlink_s,
+    width: 73,
+  },
+  {
+    id: "Colligence",
+    name: "Colligence",
+    image: Colligence,
+    image_s: Colligence_s,
+    width: 73,
+  },
+  {
+    id: "Tokenpost",
+    name: "Tokenpost",
+    image: TokenPost,
+    image_s: TokenPost_s,
+    width: 84,
+  },
+  {
+    id: "oneInch",
+    name: "1inch",
+    image: OneInch,
+    image_s: OneInch_s,
+    width: 63,
+  },
 ];
 
 export default partnersList;
